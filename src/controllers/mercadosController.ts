111ed7e9839af582bfa9209fbe79a0cfd0410bea
Jonathas Gabriel <jonathasgabriel2890@gmail.com>
2023-12-20 00:04:30 -0300

    Alteração no mercadoController + middlewares nas rotas do APP

--- src/controllers/mercadosController.ts
+++ src/controllers/mercadosController.ts
@@ -29,6 +29,65 @@ export default {
         .where('precos.produtoIdProduto IN (:...produtos)', { produtos })
         .getMany();
 
+      const mercadoProdutosFormatado = mercadosComConjuntoEspecifico.filter((mercado) => {
+        const produtosDoMercado = mercado.precos.map((preco) => preco.produto.id_produto);
+        return produtos.every((produto: number) => produtosDoMercado.includes(produto));
+      }).map((mercado) => {
+        const { id_mercado, nome_mercado, localizacao, precos, img_mercado, info, tele, endereco } = mercado;
+        const produtos = precos.map((preco) => ({
+          id_produto: preco.produto.id_produto,
+          preco: preco.preco,
+        }));
+
+        return {
+          id_mercado,
+          nome_mercado,
+          img_mercado,
+          info,
+          tele,
+          endereco,
+          localizacao,
+          produtos,
+        };
+      });
+
+      console.log(mercadoProdutosFormatado);
+
+      res.status(200).json(mercadoProdutosFormatado);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Erro ao buscar mercados com conjunto específico de produtos.' });
+    }
+  
+  },
+
+  async mercadosComPeloMenos1Produtos(req: Request, res: Response) {
+
+    try {
+      const { produtos } = req.body;
+
+      /* --Retorna os mercados que contenham pelo menos 1 dos intem inseridos--*/
+      const mercadosComConjuntoEspecifico = await AppDataSource
+        .getRepository(Mercados)
+        .createQueryBuilder('mercado')
+        .leftJoin('mercado.precos', 'precos')
+        .leftJoin('precos.produto', 'produto')
+        .select([
+          'mercado.id_mercado',
+          'mercado.nome_mercado',
+          'mercado.img_mercado',
+          'mercado.info',
+          'mercado.tele',
+          'mercado.endereco',
+          'produto.nome_produto',
+          'precos.preco',
+          'mercado.localizacao',
+          'produto.id_produto',
+          'produto.img_produto'
+        ])
+        .where('precos.produtoIdProduto IN (:...produtos)', { produtos })
+        .getMany();
+
         const mercadoProdutosFormatado = mercadosComConjuntoEspecifico.map((mercado) => {
           const { id_mercado, nome_mercado, localizacao, precos, img_mercado , info, tele, endereco } = mercado;
           const produtos = precos.map((preco) => ({
@@ -56,4 +115,5 @@ export default {
       res.status(500).json({ message: 'Erro ao buscar mercados com conjunto específico de produtos.' });
     }
   }
+
 }
--- src/routes/app/appRouter.ts
+++ src/routes/app/appRouter.ts
@@ -2,13 +2,16 @@ import { Router } from 'express';
 import categoriasController from '../../controllers/categoriasController';
 import produtosController from '../../controllers/produtosController';
 import mercadosController from '../../controllers/mercadosController';
+import authToken from '../../middlewares/authToken';
 
 const appRoutes = Router();
 
 // Rota para buscar todas as categorias
-appRoutes.get('/categorias', categoriasController.getAllCategorias);
+appRoutes.get('/categorias', authToken,categoriasController.getAllCategorias);
 // Rota para buscar todos os produtos
-appRoutes.get('/produtos', produtosController.getAllProdutos);
+appRoutes.get('/produtos', authToken,produtosController.getAllProdutos);
 // Rota para buscar todos os mercados com um conjunto de produtos especifico
-appRoutes.post('/mercadosEncontrados', mercadosController.mercadosComProdutos)
+appRoutes.post('/mercadosEncontrados',authToken,mercadosController.mercadosComProdutos)
+
+appRoutes.post('/mercadosEncontradosComMin1',authToken,mercadosController.mercadosComPeloMenos1Produtos)
 export default appRoutes;
